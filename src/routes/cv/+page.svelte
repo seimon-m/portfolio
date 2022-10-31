bbe6af27f9a3d066b556b13c4763cd33138ad664
Simon <simimueller@hotmail.com>
2022-10-31 23:57:56 +0100

    Update +page.svelte

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -3,17 +3,20 @@
 </script>
 
 <div class="cv">
-	<iframe
+	<!-- <iframe
 		title="PDF"
 		src={'/pdfjs-2.16.105-dist/web/viewer.html?file=' + '/' + 'lebenslauf.pdf'}
 		width="100%"
 		height="1000px"
-	/>
+	/> -->
+	<object data={cv} class="cv" />
 </div>
 
 <style>
 	.cv {
 		position: absolute;
+		height: -webkit-fill-available;
+		width: -webkit-fill-available;
 		top: 0;
 		left: 0;
 		bottom: 0;
