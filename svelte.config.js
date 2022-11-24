301fe03dfa70833c17092e87e38070ec3ca0d36d
Simon <simimueller@hotmail.com>
2022-11-24 20:30:55 +0100

    try with static images

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import importAssets from 'svelte-preprocess-import-assets';
 
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-	preprocess: [importAssets()]
+	}
 };
 
 export default config;
