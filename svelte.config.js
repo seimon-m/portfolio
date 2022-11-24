308cb6ef26149bfbf01402f3aee7d9f82735f057
Simon <simimueller@hotmail.com>
2022-11-24 20:07:21 +0100

    test with preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
+import importAssets from 'svelte-preprocess-import-assets';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		files: {
-			lib: 'src/lib'
-		}
-	}
+		adapter: adapter()
+	},
+	preprocess: [importAssets()]
 };
 
 export default config;
