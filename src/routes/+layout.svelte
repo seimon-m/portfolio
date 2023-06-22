62c860a3a6b744e89e03cd32b6bd8cfa43c26023
Simon <simimueller@hotmail.com>
2023-06-22 16:57:42 +0200

    minor changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
-	import '$lib/styles/reset.css';
-	import '$lib/styles/global.css';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-
-	inject({ mode: dev ? 'development' : 'production' });
+    import "$lib/styles/reset.css";
+    import "$lib/styles/global.css";
+    import { dev } from "$app/environment";
+    import { inject } from "@vercel/analytics";
+    inject({ mode: dev ? "development" : "production" });
 </script>
 
 <main>
-	<slot />
+    <slot />
 </main>
