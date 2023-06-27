5f1d72936a1629afba558cb23bbc2d0cbcbfae3b
Simon <simimueller@hotmail.com>
2023-06-27 14:56:59 +0200

    remove log

--- src/routes/(work)/[slug]/+page.js
+++ src/routes/(work)/[slug]/+page.js
@@ -1,17 +1,15 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect } from "@sveltejs/kit";
 
 export const load = async ({ fetch, params }) => {
-	const slug = params['slug'];
-	const res = await fetch(`projects/${slug}/${slug}.html`);
-	if (res.status !== 200) {
-		throw redirect(307, '/#work');
-	}
-	const post = await res.text();
+    const slug = params["slug"];
+    const res = await fetch(`projects/${slug}/${slug}.html`);
+    if (res.status !== 200) {
+        throw redirect(307, "/#work");
+    }
+    const post = await res.text();
 
-	console.log(slug);
-
-	return {
-		slug,
-		post
-	};
+    return {
+        slug,
+        post,
+    };
 };
