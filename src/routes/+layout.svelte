fd6200dd551575a40cbd291f112dc433d30498d8
Simon <simimueller@hotmail.com>
2025-10-06 16:58:20 +0200

    Add speed insights from vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
     import "$lib/styles/global.css";
     import { dev } from "$app/environment";
     import { inject } from "@vercel/analytics";
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
     inject({ mode: dev ? "development" : "production" });
+    injectSpeedInsights();
 </script>
 
 <main>
